ac6648b6c0821633129a2685a9164173968b5d7f
Naufal Asad Alhaq <asadalhaq@yandex.com>
2025-02-11 06:41:30 +0700

    feat: add Netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
+
   integrations: [
     mdx(),
     sitemap(),
@@ -18,4 +21,6 @@ export default defineConfig({
     }),
     react(),
   ],
+
+  adapter: netlify(),
 });
