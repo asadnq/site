b798784fa45dddcfc6dd24ff8135a949d16b5a73
Naufal Asad Alhaq <asadalhaq@yandex.com>
2025-02-11 20:50:46 +0700

    feat: about page

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,5 +1,4 @@
 import type { Project } from '@/types';
-import type { TechStackId } from './personal-info/tech-stack-list';
 import { TechStack } from './tech-stack';
 
 interface ProjectCardProps {
@@ -19,9 +18,11 @@ export function ProjectCard({
   return (
     <a
       href={`/projects/${id}/`}
-      className="w-full h-full min-h-[175px] rounded-md shadow-lg drop-shadow-lg bg-gradient-to-tr from-primary via-card to-secondary p-1 hover:animate-gradient">
+      // className="w-full h-full min-h-[175px] rounded-md shadow-lg drop-shadow-lg bg-gradient-to-tr from-primary via-card to-secondary p-1 hover:animate-gradient"
+      className="w-full h-full min-h-[175px] rounded-md shadow-lg drop-shadow-lg p-1 bg-card/50"
+      >
       {/* <a href={`/projects/${id}`}> */}
-      <div className="flex flex-col justify-between items-stretch h-full px-4 py-2.5 rounded-lg bg-background/85 backdrop-blur-xl">
+      <div className="flex flex-col justify-between items-stretch h-full px-4 py-2.5 rounded-lg bg-background/75 backdrop-blur-xl">
         <div className="flex flex-col gap-y-4">
           <h2 className="text-2xl font-bold text-foreground">{title}</h2>
           <p className="text-muted-foreground text-sm">{description}</p>
